e71bee079596659382a5bc90c73a92d7fa3e7d7a
Matey-Nikolov <matey.nikolov@mail.bg>
2021-05-07 18:51:57 +0300

    Add project files.

---
+++ NewSoftUniBuilding/Program.cs
@@ -0,0 +1,33 @@
+﻿Теnamespace NewSoftUniBuilding
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int width = n;
+            int height = n + n / 2;
+
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    bool isStripe = (j + i) % 4 == 0;
+
+                    if (isStripe)
+                    {
+                        Console.Write("#");
+                    }
+                    else
+                    {
+                        Console.Write(".");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Softuniada2020/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Softuniada2020
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            decimal startingSalary = decimal.Parse(Console.ReadLine());
+            decimal monthlyExpense = decimal.Parse(Console.ReadLine());
+            decimal salaryUP = decimal.Parse(Console.ReadLine());
+            decimal dreamCarMoney = decimal.Parse(Console.ReadLine());
+            int month = int.Parse(Console.ReadLine());
+
+
+            decimal earned = 0;
+            for (int i = 1; i <= month; i++)
+            {
+                if (i == 1)
+                    startingSalary = startingSalary;
+                else
+                    startingSalary = startingSalary + salaryUP;
+
+                earned += startingSalary;
+            }
+
+            Console.WriteLine(earned);
+            decimal expenseTotal = month * monthlyExpense;
+            decimal TotalSum = earned - expenseTotal;
+
+            if (TotalSum >= dreamCarMoney)
+                Console.WriteLine("Have a nice ride!");
+            else
+                Console.WriteLine("Work harder!");
+
+        }
+    }
+}
